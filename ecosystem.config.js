d9bd325bb6d4ed3a29b140f09fd8e2cbbaa0055e
larryro <371767072@qq.com>
2022-09-27 17:03:57 +0800

    fix bug

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,32 @@
 module.exports = {
   apps: [
     {
+      name: 'theta-data-api',
+      script: './dist/main.js',
+      instances: 8,
+      exec_mode: 'cluster',
+      autorestart: false,
+      watch: false,
+      max_memory_restart: '4G',
+      increment_var: 'PORT',
+      instance_var: 'INSTANCE_ID',
+      env_test: {
+        autorestart: true,
+        NODE_ENV: 'test',
+        PORT: 3000,
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 3000,
+        autorestart: true,
+      },
+      env_dev: {
+        NODE_ENV: 'dev',
+        PORT: 3000,
+        exec_mode: 'fork',
+      },
+    },
+    {
       name: 'analyse',
       script: './dist/analyse.js',
       instances: 1,
