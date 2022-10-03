dbc28801a1565fc0465e6e2520114a2951142932
larryro <371767072@qq.com>
2022-10-03 09:26:38 +0800

    make nft analyse independly

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -47,5 +47,26 @@ module.exports = {
         exec_mode: 'fork',
       },
     },
+    {
+      name: 'analyse-nft',
+      script: './dist/analyse-nft.js',
+      instances: 1,
+      exec_mode: 'cluster',
+      autorestart: false,
+      watch: false,
+      max_memory_restart: '2G',
+      env_test: {
+        NODE_ENV: 'test',
+        autorestart: true,
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        autorestart: true,
+      },
+      env_dev: {
+        NODE_ENV: 'dev',
+        exec_mode: 'fork',
+      },
+    },
   ],
 };
